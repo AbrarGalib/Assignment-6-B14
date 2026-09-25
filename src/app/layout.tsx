bb20802f2876dba0a8045b992d55c068d9160840
AbrarGalib <abrargalib00@gmail.com>
2026-09-25 23:52:54 +0600

    footer added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import { WorkoutProvider } from "./context/WorkoutContext";
+import Footer from "./components/Footer";
 
 
 const geistSans = Geist({
@@ -32,7 +33,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           
           <Navbar />
           <main className="flex-grow">{children}</main>
-          {/* <Footer /> */}
+      <Footer />
           
         </WorkoutProvider>
       
